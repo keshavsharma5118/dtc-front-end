adbc473b9ba7f830a39ee97112ec1baef63be4a6
keshavsharma5118 <keshav.s5118@gmail.com>
2024-09-02 03:03:52 +0530

    setuped profile , link, etc

---
+++ public/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.back-button').addEventListener('click', function() {
+        window.history.back();
+    });
+});
