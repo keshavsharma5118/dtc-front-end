0bb9dc6c1058c11ab5d872f3788fdfa7c722f799
keshavsharma5118 <keshav.s5118@gmail.com>
2024-09-01 22:53:38 +0530

    added scheduler all files

--- public/js/datetime.js
+++ public/js/datetime.js
@@ -1,11 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const dateTimeElements = document.querySelectorAll("#date-time");
+function updateDateTime() {
+    const dateTimeElements = document.querySelectorAll("#datetime");
     const now = new Date();
     const options = { day: '2-digit', month: 'short', year: 'numeric' };
     const formattedDate = now.toLocaleDateString('en-GB', options);
-    const formattedTime = now.toTimeString().split(' ')[0].slice(0, 5);
+    const formattedTime = now.toTimeString().split(' ')[0].slice(0, 8);  // Includes HH:MM:SS
 
     dateTimeElements.forEach(element => {
         element.textContent = `${formattedDate.replace(/ /g, '-')} || ${formattedTime}`;
     });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateDateTime();
+    setInterval(updateDateTime, 1000);  // Update every second
 });
